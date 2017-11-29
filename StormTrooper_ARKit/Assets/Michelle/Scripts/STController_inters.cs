03cd320dfdbeed572cd3338192fd312d7c4ec9c5
Michelle Hessel <michhessel@gmail.com>
2017-11-28 21:12:43 -0500

    Uploaded complete project Unity 2017.3 (beta)

---
+++ StormTrooper_ARKit/Assets/Michelle/Scripts/STController_inters.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class STController_inters : MonoBehaviour {
+
+	Animator animST;
+	public GameObject Target;
+	public Button dance;
+	public Button idle;
+	public float size;
+
+	// Use this for initialization
+	void Start () {
+		animST = Target.GetComponent<Animator> ();
+		dance.onClick.AddListener (danceST);
+		idle.onClick.AddListener (idleST);
+	}
+		
+
+	void danceST(){
+		animST.SetBool ("isDancing", true);
+		animST.SetBool ("isIdle", false);
+	}
+
+	void idleST(){
+		animST.SetBool ("isDancing", false);
+		animST.SetBool ("isIdle", true);
+	}
+
+	public void changeSize (float newSize){
+		size = newSize;
+		Target.transform.localScale = new Vector3 (newSize, newSize, newSize);	
+	}
+}
---
+++ StormTrooper_ARKit/Assets/Michelle/Scripts/StormTrooperController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StormTrooperController : MonoBehaviour {
+
+	Animator myAnim;
+
+	public Button Dance;
+	public Button Idle;
+	public float size;
+	public GameObject Target;
+
+
+	void Start () {
+		myAnim = Target.GetComponent<Animator> ();
+		Dance.onClick.AddListener (dancebaby);
+		Idle.onClick.AddListener (idlebaby);
+	}
+
+
+	void dancebaby(){
+		myAnim.SetBool ("isDancing", true);
+		myAnim.SetBool ("isIdle", false);
+	}
+
+
+	void idlebaby(){
+		myAnim.SetBool ("isIdle", true);
+		myAnim.SetBool ("isDancing", false);
+	}
+
+
+
+	public void sizeChange(float newSize){
+		size = newSize;
+		Target.transform.localScale = new Vector3 (newSize, newSize, newSize);
+	}
+		
+
+
+}
